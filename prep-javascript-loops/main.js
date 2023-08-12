179572acc0646908af4a3aef10ba4e9ad2c25ecb
Tiana Lopez <tianadavin@gmail.com>
2023-08-12 18:44:31 +0000

    Completed prep-javascript-loops exercise

---
+++ prep-javascript-loops/main.js
@@ -0,0 +1,63 @@
+// create your loops here.
+function whileLoop1() {
+  const newArray = [];
+  let i = 0;
+  while (i < 10) {
+    newArray.push(i);
+    i++;
+  }
+  return newArray;
+}
+console.log('whileLoop1 output', whileLoop1());
+
+function whileLoop2() {
+  const newArray = [];
+  let i = 0;
+  while (i < 10) {
+    newArray.push(2 * i);
+    i++;
+  }
+  return newArray;
+}
+console.log('whileLoop2 output', whileLoop2());
+
+function forLoop1() {
+  const newArray = [];
+  for (let i = 0; i < 10; i++) {
+    newArray.push(i);
+  }
+  return newArray;
+}
+console.log('forLoop1 output', forLoop1());
+
+function forLoop2() {
+  for (let i = 100; i > 0; i--) {
+    console.log(`Time till explosion ${i}!`);
+  }
+}
+console.log(forLoop2());
+
+function forInLoop1(object) {
+  const newArray = [];
+  for (const property in object) {
+    newArray.push(property);
+  }
+  return newArray;
+}
+
+const object = {
+  name: 'Ada LoveLace',
+  age: 'classic',
+  hobby: 'computation',
+  invention: 'analytical engine',
+};
+console.log(forInLoop1(object));
+
+function forInLoop2(object) {
+  const newArray = [];
+  for (const property in object) {
+    newArray.push(object[property]);
+  }
+  return newArray;
+}
+console.log(forInLoop2(object));
